fff01616ccb97d4c004eaa980cb9ccfbe2c09cd5
amanraj603 <aman603raj@gmail.com>
2025-12-30 14:54:40 +0530

    Formating doc

--- src/models/user.js
+++ src/models/user.js
@@ -15,19 +15,20 @@ const userSchema = new Schema(
       trim: true,
       lowercase: true,
       required: true,
-      validate(value){
-        if(!validator.isEmail(value)){
+      validate(value) {
+        if (!validator.isEmail(value)) {
           throw new Error("Email is not valid");
         }
-      }
+      },
     },
     password: {
       type: String,
       required: true,
-      validate(value){
-        if(!validator.isStrongPassword(value))
-            throw new Error("Password is not strong enough");
-      }
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password is not strong enough");
+        }
+      },
     },
     age: {
       type: Number,
@@ -45,11 +46,11 @@ const userSchema = new Schema(
       type: String,
       default:
         "https://static.vecteezy.com/system/resources/previews/048/216/761/non_2x/modern-male-avatar-with-black-hair-and-hoodie-illustration-free-png.png",
-        validator(value){
-            if(!validator.isURL(value)){
-                throw new Error("Photo URL is not valid");
-            }
+      validator(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Photo URL is not valid");
         }
+      },
     },
     about: {
       type: String,
